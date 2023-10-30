f0f0c104de350afdb2b6fd3123baec92e1b2c159
Yunmanjecc <j.clemente3@alumnos.santotomas.cl>
2023-10-30 16:05:22 -0300

    se construye cómo será la pantalla de detección de colores

--- app/src/main/java/juan/clemente/primerainterfaz/detectar.java
+++ app/src/main/java/juan/clemente/primerainterfaz/detectar.java
@@ -1,10 +1,33 @@
 package juan.clemente.primerainterfaz;
 
-import androidx.appcompat.app.AppCompatActivity;
+//Utilizar proyecto futuro de arduino
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.palette.graphics.Palette;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 public class detectar extends AppCompatActivity {
+    // Cargar la imagen
+    Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.my_image);
+
+    // Crear una instancia de Palette
+    Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+        @Override
+        public void onGenerated(@NonNull Palette palette) {
+            // Obtener el color dominante
+            int dominantColor = palette.getDominantColor(0x000000);
+
+            // Convertir el color a formato hexadecimal
+            String hexColor = String.format("#%06X", (0xFFFFFF & dominantColor));
+
+            // Guardar el color en una variable
+            String myColor = hexColor;
+        }
+    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
